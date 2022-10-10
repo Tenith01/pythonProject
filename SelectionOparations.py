9aadddfe8776261f8ac794cf5edda0b4f608ac8d
fct <hasintha199@gmail.com>
2022-10-10 12:31:50 +0530

    Add Initial Project

---
+++ SelectionOparations.py
@@ -0,0 +1,5 @@
+def drive(car_seed):
+    if car_seed>100:
+        print("too Fast!")
+    else:
+        print("high speed")
---
+++ main.py
@@ -0,0 +1,7 @@
+def get_age(name):
+    ages = {"teni": 12, "as": 32, "sachi": 12}
+    return (ages[name])
+
+
+a = get_age("teni")
+print(a)
